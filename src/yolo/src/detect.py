26e2992fd4acab17ea552e9898f8a17c83ad01c7
lc252 <lc252@uowmail.edu.au>
2023-06-26 14:02:17 +1000

    added pc callback, cleaned inference_cb

--- src/yolo/src/detect.py
+++ src/yolo/src/detect.py
@@ -4,7 +4,7 @@ import rospy
 from ultralytics import YOLO
 import numpy as np
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
 
 
 
@@ -16,24 +16,22 @@ class Detector:
         if self.params["cuda"]:
             self.model.to("cuda")
         # image subscriber
-        self.sub = rospy.Subscriber('camera/color/image_raw', Image, self.new_image_cb)
+        self.img_sub = rospy.Subscriber("camera/color/image_raw", Image, self.image_cb)
         self.img = np.zeros(shape=(480,640,3))
+        # pointcloud subscriber
+        self.pc_sub = rospy.Subscriber("camera/")
         # timer callback to perform inference. on a separate thread for efficiency
         self.timer = rospy.timer.Timer(rospy.Duration(1/30), self.inference_cb)
 
-    def new_image_cb(self, img_msg: Image):
+    def image_cb(self, img_msg: Image):
         self.img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
 
-    def inference_cb(self, _):
+    def pc_callback(self, pc2_msg: PointCloud2):
+        pass
+
+    def inference_cb(self, timer):
         # do detection
-        results = self.model(self.img)
-        # render result in cv     
-        if True: #self.params["render"]:
-            self.render_cb(results)
-    
-    def render_cb(self, results):
-        cv2.imshow('Stream', results[0].plot())
-        cv2.waitKey(1)
+        results = self.model.predict(self.img, show=self.params["render"])
 
 
 
