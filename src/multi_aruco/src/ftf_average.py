f593b7d307e44403a174caefae68c025c5c4261e
lc252 <lc252@uowmail.edu.au>
2023-08-20 13:19:17 +1000

    updated to match tested code in ros-playground

--- src/multi_aruco/src/ftf_average.py
+++ src/multi_aruco/src/ftf_average.py
@@ -3,8 +3,9 @@
 import rospy
 import numpy as np
 from fiducial_msgs.msg import FiducialTransform, FiducialTransformArray
-from geometry_msgs.msg import Vector3, Quaternion, PoseWithCovarianceStamped
-from tf import transformations
+from geometry_msgs.msg import Vector3, Quaternion, PoseWithCovarianceStamped, TransformStamped
+from scipy.spatial.transform import Rotation as R
+import tf2_ros as tf2
 
 
 
@@ -27,27 +28,57 @@ class ftf_average():
 
         ftf : FiducialTransform
         for ftf in ftf_arr.transforms:
-            euler = transformations.euler_from_quaternion([ftf.transform.rotation.x, ftf.transform.rotation.y, ftf.transform.rotation.z, ftf.transform.rotation.w,])
-            roll.append(euler[0])
-            pitch.append(euler[1])
-            yaw.append(euler[2])
+            # get the known base -> aruco transform
+            ar_base_TF : TransformStamped
+            ar_base_TF = self.buffer.lookup_transform(f"aruco_{ftf.fiducial_id}_known", "base_link", rospy.Time())
+            # extract the translation and rotation
+            ar_base_t = ar_base_TF.transform.translation
+            ar_base_q = ar_base_TF.transform.rotation
+            ar_base_r = R.from_quat([ar_base_q.x, ar_base_q.y, ar_base_q.z, ar_base_q.w])
+            # build the transform matrix
+            ar_base_M = np.identity(4)
+            ar_base_M[0:3,0:3] = ar_base_r.as_matrix()
+            ar_base_M[0:3, 3] = np.array([ar_base_t.x, ar_base_t.y, ar_base_t.z])
 
-            x.append(ftf.transform.translation.x)
-            y.append(ftf.transform.translation.y)
-            z.append(ftf.transform.translation.z)
-        
+            # get the camera -> aruco transform
+            cam_ar_t = ftf.transform.translation
+            cam_ar_q = ftf.transform.rotation
+            cam_ar_r = R.from_quat([cam_ar_q.x, cam_ar_q.y, cam_ar_q.z, cam_ar_q.w])
+            # build the transform matrix
+            cam_ar_M = np.identity(4)
+            cam_ar_M[0:3,0:3] = cam_ar_r.as_matrix()
+            cam_ar_M[0:3, 3] = np.array([cam_ar_t.x, cam_ar_t.y, cam_ar_t.z])
+
+            # get the camera -> base transform
+            cam_base_M = np.matmul(cam_ar_M, ar_base_M)
+            # invert the transform to get base -> camera
+            base_cam_M = np.linalg.inv(cam_base_M)
+            # extract the rotation matrix
+            base_cam_r = R.from_matrix(base_cam_M[0:3,0:3])
+            # extract the euler angles
+            base_cam_eul = base_cam_r.as_euler("xyz")  
+            
+            # append rotation and translation to lists for averaging
+            roll.append(base_cam_eul[0])
+            pitch.append(base_cam_eul[1])
+            yaw.append(base_cam_eul[2])
+            x.append(base_cam_M[0,3])
+            y.append(base_cam_M[1,3])
+            z.append(base_cam_M[2,3])
+
+        # calculate the covariance matrix
         cov = np.array([[np.var(x), 0, 0, 0, 0, 0],
                         [0, np.var(y), 0, 0, 0, 0],
                         [0, 0, np.var(z), 0, 0, 0],
                         [0, 0, 0, np.var(roll), 0, 0],
                         [0, 0, 0, 0, np.var(pitch), 0],
                         [0, 0, 0, 0, 0, np.var(yaw)]])
-        cov = cov**2
 
-        q = transformations.quaternion_from_euler(np.mean(roll), np.mean(pitch), np.mean(yaw))
+        # calculate the average rotation and translation
+        q = R.from_euler("xyz", [np.mean(roll), np.mean(pitch), np.mean(yaw)]).as_quat()
         pwcs.pose.pose.position = Vector3(np.mean(x), np.mean(y), np.mean(z))
         pwcs.pose.pose.orientation = Quaternion(*q)
-        pwcs.pose.covariance = cov.flatten()
+        # pwcs.pose.covariance = cov.flatten()
 
         # publish
         self.pose_pub.publish(pwcs)
