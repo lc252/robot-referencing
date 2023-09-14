9a28ccdbb6223c850619dc8b718ac25a9110e31c
lc252 <lc252@uowmail.edu.au>
2023-09-14 16:54:18 +1000

    use tf transformations

--- src/robot_referencing/src/plot.py
+++ src/robot_referencing/src/plot.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.spatial.transform import Rotation as R
 import numpy as np
+from tf import transformations
 
 
-df = pd.read_csv("/home/lachl/measurements/data.csv")
+df = pd.read_csv("/home/lachl/measurements/multi_aruco_tf_euler.csv")
 print(df.head())
 df["roll"] = np.zeros(len(df))
 df["pitch"] = np.zeros(len(df))
@@ -12,8 +13,9 @@ df["yaw"] = np.zeros(len(df))
 
 # convert quaternions to euler angles
 for i in range(len(df)):
-    r = R.from_quat([df["qx"][i], df["qy"][i], df["qz"][i], df["qw"][i]])
-    roll, pitch, yaw = r.as_euler("xyz", degrees=True)
+    # r = R.from_quat([df["qx"][i], df["qy"][i], df["qz"][i], df["qw"][i]])
+    # roll, pitch, yaw = r.as_euler("xyz", degrees=True)
+    roll, pitch, yaw = transformations.euler_from_quaternion([df["qx"][i], df["qy"][i], df["qz"][i], df["qw"][i]], axes="sxyz")
     df["roll"][i] = roll
     df["pitch"][i] = pitch
     df["yaw"][i] = yaw
@@ -23,8 +25,8 @@ for k in df.keys():
     df[k] = df[k] - df[k].mean()
     print("range", k, "=", max(df[k]) - min(df[k]))
 
-# plot histograms of x,y,z and qx,qy,qz,qw
-fig, axs = plt.subplots(2, 3)
+# plot histograms of x,y,z and r,p,y and qx,qy,qz,qw
+fig, axs = plt.subplots(3, 4)
 axs[0, 0].hist(df["x"], bins=100)
 axs[0, 0].set_title("x")
 axs[0, 1].hist(df["y"], bins=100)
@@ -37,6 +39,14 @@ axs[1, 1].hist(df["pitch"], bins=100)
 axs[1, 1].set_title("pitch")
 axs[1, 2].hist(df["yaw"], bins=100)
 axs[1, 2].set_title("yaw")
+# axs[2, 0].hist(df["qx"], bins=100)
+# axs[2, 0].set_title("qx")
+# axs[2, 1].hist(df["qy"], bins=100)
+# axs[2, 1].set_title("qy")
+# axs[2, 2].hist(df["qz"], bins=100)
+# axs[2, 2].set_title("qz")
+# axs[2, 3].hist(df["qw"], bins=100)
+# axs[2, 3].set_title("qw")
 
 print(df.describe())
 
